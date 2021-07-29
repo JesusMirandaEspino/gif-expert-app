e5dfa0ac8b7bb68b69a9db16cf2675865d895072
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-28 20:48:56 -0500

    crear documento principal

---
+++ src/GiftAppExpert.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const GiftAppExpert  = (   ) => {
+
+   // fuera de codigo const saludo = 'Hola mundo desde react';
+
+    return (
+        <section className="fila">  
+            <div className="contenedor1">
+                <div className="col-full-12">
+                    <h1> GiftAppExpert </h1>
+                    <hr></hr>
+                </div>
+            </div>
+        </section>
+    );
+    
+}
+
+export default GiftAppExpert;
