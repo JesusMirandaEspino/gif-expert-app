1826974b4b3d6c4bee266df6936b2f0492f5a61c
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-28 21:17:11 -0500

    setstate return array

--- src/GiftAppExpert.js
+++ src/GiftAppExpert.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const GiftAppExpert  = (   ) => {
 
-   // fuera de codigo const saludo = 'Hola mundo desde react';
+   // arreglo de categorias
+
+    // no se declara asi const categorias = [ 'One Punch', 'Samurai X', 'Dragon Ball' ];
+    const [ categories, setCategories ] = useState( [ 'One Punch', 'Samurai X', 'Dragon Ball' ] ); 
+
+    const handleAdd = () => {
+        setCategories( cat => [ ...cat,  'Demon Slayer'] );
+    }
 
     return (
         <section className="fila">  
             <div className="contenedor1">
+
                 <div className="col-full-12">
                     <h1> GiftAppExpert </h1>
                     <hr></hr>
                 </div>
+
+                <div className="col-full-12">
+                    <ol>
+                        {
+                            categories.map( category => {
+                                return <li  key={category} > { category } </li>
+                            } )
+                        }
+                    </ol>
+                </div>
+
+                <div className="col-full-12">
+                        <button onClick={ handleAdd } >Agregar</button>
+                </div>
+
             </div>
         </section>
     );
