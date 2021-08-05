ab6e955dd7f21cd85b0347805019dea087385934
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-04 22:09:43 -0500

    test img y p

--- src/test/GifGridItem.test.js
+++ src/test/GifGridItem.test.js
@@ -9,17 +9,29 @@ describe( 'Pruebas en el archivo <GifGridItem />', () => {
 
         const title = 'Es un titulo';
         const url = 'https://localhostesunaurl';
-
-    test( 'Debe mostrar el componente correctamente', () => { 
-
         const wrapper = shallow( <GifGridItem  title={title} url={url} /> );
 
+    test( 'Debe mostrar el componente correctamente', () => { 
 
         expect( wrapper ).toMatchSnapshot();
 
     });
 
 
+    test( 'Debe tener un parrafo con el texto con el title', () => { 
+
+            const p = wrapper.find( 'p' );
+            expect( p.text().trim() ).toBe( title );
+
+    });
+
+    test( 'Debe tener la url de la imagen y el alt', () => { 
+
+            const img = wrapper.find( 'img' );
+            expect( img.prop('src') ).toBe( url );
+            expect( img.prop('alt') ).toBe( title );
+
+    });
 
 
 } );
