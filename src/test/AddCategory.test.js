cf3c60106ba857070b32ab98beef5f289b8e31fa
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-09 21:16:49 -0500

    test input value empty

--- src/test/AddCategory.test.js
+++ src/test/AddCategory.test.js
@@ -5,38 +5,30 @@ import { AddCategory }  from '../components/AddCategory';
 
 describe( 'Pruebas con el componente <AddCategory />', () => {
 
-
     const  setCategories = jest.fn();
 
-    let wrappers = shallow( <AddCategory  setCategories={ setCategories } />    );
+    let wrappers = shallow( <AddCategory  setCategories={ setCategories } /> );
 
     beforeEach( () => {
         jest.clearAllMocks();
-        wrappers = shallow( <AddCategory  setCategories={ setCategories } />    );
-
+        wrappers = shallow( <AddCategory  setCategories={ setCategories } />  );
     } );
 
     test('debe de mostrarse correctamente ', () => {
         
 
-
-
         expect( wrappers ).toMatchSnapshot();
 
     });
 
-
-    
     test('debe de cambiar la caja de texto  ', () => {
         
-
         const input  = wrappers.find( 'input' );
         const value = 'Hola Mundo';
 
         input.simulate( 'change', { target: { value } } );
 
     });
-
 
     test( 'NO debe de postear la informacion con submint', () => {
 
@@ -46,8 +38,20 @@ describe( 'Pruebas con el componente <AddCategory />', () => {
 
     } );
 
-    
+
+    test( 'Debe llamar al setCategories y limpiar la caja de texto', () => {
+
+        const value = 'hola mundo';
+
+        wrappers.find( 'input' ).simulate( 'change', { target: {value} } );
+
+        wrappers.find( 'form' ).simulate( 'submit', { preventDefault(){} } );
+
+        expect( setCategories ).toHaveBeenCalled();
+
+        expect(  wrappers.find( 'input' ).prop( 'value' ) ).toBe('');
 
 
+    } );
 
 } );
