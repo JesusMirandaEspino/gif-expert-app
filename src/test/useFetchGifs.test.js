082b3720745cdbc8a01f90ae38c75a6fa6987f18
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-11 21:32:31 -0500

    hooks test

--- src/test/useFetchGifs.test.js
+++ src/test/useFetchGifs.test.js
@@ -1,15 +1,19 @@
 import { useFetchGifs } from '../hooks/useFetchGifs';
+import { renderHook } from '@testing-library/react-hooks';
 
 describe('pruebas con hooks useFetchGifs', () => {
 
     test('Debe de retorna el estado inicial ', () => {
 
-        const { data, loading } = useFetchGifs( 'one punch');
+        const { result } = renderHook( () => useFetchGifs( 'One Punch')  );
 
+        const  { data, loading } = result.current;
 
-        console.log( data, loading );
+        expect( data ).toEqual([]);
+        expect( loading ).toBe(true);
 
+       //  constante del valor de hook  const { data, loading } = useFetchGifs( 'one punch');
+
+        //  imprime valores de constante hooks console.log( data, loading );
     });
-
-
 });
