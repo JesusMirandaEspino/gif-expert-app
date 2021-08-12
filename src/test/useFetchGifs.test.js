89bc95e0f5f5f79f9785037b3674c254f5ad0a7a
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-11 21:46:25 -0500

    testing hooks complete

--- src/test/useFetchGifs.test.js
+++ src/test/useFetchGifs.test.js
@@ -3,11 +3,14 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('pruebas con hooks useFetchGifs', () => {
 
-    test('Debe de retorna el estado inicial ', () => {
+    test('Debe de retorna el estado inicial ', async() => {
 
-        const { result } = renderHook( () => useFetchGifs( 'One Punch')  );
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch')  );
+        
 
         const  { data, loading } = result.current;
+
+        await waitForNextUpdate();
 
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
@@ -16,4 +19,18 @@ describe('pruebas con hooks useFetchGifs', () => {
 
         //  imprime valores de constante hooks console.log( data, loading );
     });
+
+    test( 'debe retornar un arreglo de img y el loading en false', async() => {
+
+
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch')  );
+        await waitForNextUpdate();
+
+        const  { data, loading } = result.current;
+
+        expect( data.length).toBe(10);
+        expect( loading ).toBe(false);
+
+    } );
+
 });
